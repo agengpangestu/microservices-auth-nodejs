4027ce4cfb155a1b16885bc46d1f19ece07a344a
agengpangestu <kapalmabur002@gmail.com>
2024-09-18 16:06:20 +0700

    change name

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
-import * as UserService from "../services/auth.service";
+import * as AuthService from "../services/auth.service";
 
 export const Register = async (req: Request, res: Response): Promise<void> => {
   try {
-    const user_created = await UserService.Register(req.body);
+    const user_created = await AuthService.Register(req.body);
     res.status(201).json(user_created);
   } catch (error: any) {
     res.status(500).json({ error: error.message });
@@ -14,7 +14,7 @@ export const Login = async (req: Request, res: Response): Promise<void> => {
   const { email, password } = req.body;
 
   try {
-    const { user, token } = await UserService.Login(email, password);
+    const { user, token } = await AuthService.Login(email, password);
     res.status(200).json({ user, token });
   } catch (error: any) {
     res.status(500).json({ error: error.message });
